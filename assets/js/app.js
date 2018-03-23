c208dfad7944e41a9eb031c7fe00714a1d78fa76
syk1k <dambemondo@gmail.com>
2018-03-23 02:38:37 -0400

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,103 @@
+(function () {
+    // Creation of the base layers
+
+    var univSearch = [];
+
+    var options = {
+        valueNames:['name']
+    };
+
+
+
+    var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    });
+
+    var cartoLight = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
+    });
+
+    var mapbox = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'streets-v9',
+        accessToken: 'pk.eyJ1Ijoic3lrMWsiLCJhIjoiY2plb2JqMHllNGYydjJ3cGVmMnE2aHlkYSJ9.uXv_J38Ndp0_aHJ0r9zP4A'
+    });
+
+    // Creation of the overlays
+
+    var univLayer = L.geoJSON(univ, {
+        onEachFeature : function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+            layer.on({
+                click : function () {
+                    map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 17);
+                }
+            });
+            univSearch.push({name:layer.feature.properties.name});
+            $("#univList tbody").append("<tr class='feature-row' id="+L.stamp(layer)+" lat="+layer.getLatLng().lat+" long="+layer.getLatLng().lng+">"+"<td class='bg-light name'>"+layer.feature.properties.name+"</td>"+"</tr>");
+        }
+    });
+
+    var sideBarUnivList = new List("univList", options);
+
+    // Creation of the markercluster to hold all markers
+    var markers = new L.markerClusterGroup();
+    markers.addLayer(univLayer);
+
+    var map = L.map("map", {
+        center : [8.695, 2.395],
+        zoom : 7,
+        zoomControl: false,
+        attributionControl: false,
+        layers: [osm, markers]
+    });
+
+    L.marker([50.5, 30.5]).addTo(map);
+
+    var baseMaps = {
+        "OpenStreetMap": osm,
+        "CartoDB": cartoLight,
+        "MapBox": mapbox
+    };
+
+    var overlayMaps = {
+        "Universities": markers
+    };
+
+    var controlOptions = {
+        collapsed : false
+    };
+
+    // Creation of the layer Control
+	L.control.layers(baseMaps,overlayMaps, controlOptions).addTo(map);
+	
+	// Creation of a zoom Controle and adding it to the map
+	L.control.zoom({
+		position: 'bottomleft'
+	}).addTo(map);
+	
+	// Creation of the attribution controle and adding it to the map
+	L.control.attribution({
+		prefix: '<a href="kalamar.tg">Kalamar</a>'
+	}).addTo(map);
+	
+	// Manage the event on Click to the sidebar
+	
+	function sidebarClick(id){ // This function exexute the click on a layer specifically by the id
+		var layer = markers.getLayer(id);
+		map.setView([layer.getLatLng().lat, layer.getLatLng().lng, 17]);
+		layer.fire('click');
+	}
+	
+	$(document).on("click", ".feature-row", function(e) { // This function execute a click on the document take the id of the element Clicked and pass it to the sidebarClick function
+		sidebarClick(parseInt($(this).attr("id"), 10));
+	});
+
+	$(document).on("click", function () {
+        alert(map.getBounds().toBBoxString());
+    });
+	
+
+})();
---
+++ data/university_togo.js
@@ -0,0 +1,23 @@
+var univ = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "@id": "node\/3537301588", "amenity": "university", "name": "Institut Africain d’Administration et d’Etudes Technologiques" }, "geometry": { "type": "Point", "coordinates": [ 1.2145105, 6.1646274 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/3767548830", "amenity": "university", "name": "Université de Lomé" }, "geometry": { "type": "Point", "coordinates": [ 1.2150047, 6.1710179 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/3779983557", "amenity": "university", "name": "langcenter international" }, "geometry": { "type": "Point", "coordinates": [ 1.2158516, 6.1332602 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/3860728666", "amenity": "university", "name": "DEFITECH II CAMPUS AWOUDJA" }, "geometry": { "type": "Point", "coordinates": [ 1.2514832, 6.1913859 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/4405257894", "amenity": "university", "name": "Business School Institut Supérieur" }, "geometry": { "type": "Point", "coordinates": [ 1.189951, 6.2087622 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/4560399594", "amenity": "university", "name": "Université de Lomé" }, "geometry": { "type": "Point", "coordinates": [ 1.215714, 6.1736838 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/4588861973", "amenity": "university", "name": "Ism adonaï" }, "geometry": { "type": "Point", "coordinates": [ 1.1918449, 9.5499902 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/4620831287", "amenity": "university", "name": "Institut Confucius" }, "geometry": { "type": "Point", "coordinates": [ 1.212351, 6.1700116 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/4621166438", "amenity": "university", "name": "École Supérieure de Technologie Biologique et Alimentaire" }, "geometry": { "type": "Point", "coordinates": [ 1.2130237, 6.1776567 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/4621166439", "amenity": "university", "name": "École des Assistants Médicaux" }, "geometry": { "type": "Point", "coordinates": [ 1.2124268, 6.1773543 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/4621192540", "amenity": "university", "name": "Faculté des Sciences de la Santé" }, "geometry": { "type": "Point", "coordinates": [ 1.2130138, 6.1771101 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/4920460071", "amenity": "university", "name": "Université de Kara" }, "geometry": { "type": "Point", "coordinates": [ 1.2082252, 9.5310314 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/4920460160", "amenity": "university", "name": "Monseigneur BAKPESSI" }, "geometry": { "type": "Point", "coordinates": [ 1.2003878, 9.5461866 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/5105497979", "amenity": "university", "name": "ISM Adonaï" }, "geometry": { "type": "Point", "coordinates": [ 1.2322533, 6.1547363 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/5105813277", "amenity": "university", "name": "Ecole Supérieure des Ponts et Chaussées" }, "geometry": { "type": "Point", "coordinates": [ 1.2433945, 6.1739209 ] } },
+{ "type": "Feature", "properties": { "@id": "node\/5324525220", "amenity": "university", "name": "Ecole Normale supérieure." }, "geometry": { "type": "Point", "coordinates": [ 1.1247149, 7.5350496 ] } }
+]
+};
